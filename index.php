07dcc46daee5c99a9a6eb92e3e5761086a9aa109
TaiPham04 <taipham1290@gmail.com>
2025-12-17 12:50:05 +0700

    upd

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <H1>Xin chào OSS</H1>
-<p>lorem</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolorem reprehenderit, aut, alias adipisci veniam magnam incidunt corporis esse fuga a doloremque expedita quidem ex vitae, vero impedit maiores ipsam?</p>
 <h2>helloS</h2>
+<h1>cc</h1>
