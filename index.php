70a94a027733d01782f63ed39f3f99189ebeae3b
TaiPham04 <taipham1290@gmail.com>
2025-12-18 12:59:21 +0700

    upd

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
-<H1>Xin chào OSS</H1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolorem reprehenderit, aut, alias adipisci veniam magnam incidunt corporis esse fuga a doloremque expedita quidem ex vitae, vero impedit maiores ipsam?</p>
-<h2>helloS</h2>
-<h1>Hôm nay là thứ 5 ca 2</h1>
-<h3>Chào mừng bạn đến với OSS</h3>
+<h1>Phạm Hữu Tài - Chiều thứ 5</h1>
